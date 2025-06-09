b226f01e3f7871d27f592e9cdb2539cd1ad76d32
Eduardo Ortiz Espinoza <kanon488@hotmail.com>
2025-06-08 23:57:35 -0600

    video placeholder and display buttons

---
+++ lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:toktik/presentation/widgets/shared/video_buttons.dart';
+import 'package:toktik/video_post.dart';
+
+class VideoScrollableView extends StatelessWidget {
+  final List<VideoPost> videos;
+
+  VideoScrollableView({
+    super.key,
+    required this.videos
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView.builder(
+      scrollDirection: Axis.vertical,
+      itemCount: videos.length,
+      itemBuilder: (context,index){
+        final VideoPost videoPost = videos[index];
+
+        return Stack(
+          children: [
+            Positioned(
+              bottom: 40,
+              right: 40,
+              child: VideoButtons(video: videoPost)
+              )
+          ],
+        );
+      },
+    );
+  }
+}
